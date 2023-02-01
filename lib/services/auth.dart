f5a6c50220a90d5874b38fb8e2b06998c11fd3c4
Alxbry28 <46429925+Alxbry28@users.noreply.github.com>
2023-02-01 23:32:36 +0800

    [tutorial7] setup Stream for auth change

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,12 +2,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:practicefirebase/models/user.dart';
 
 class AuthService {
-
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   //Create user obj based on FirebaseUser
-  MyUser? _userFromFirebaseUser(User user){
+  MyUser? _userFromFirebaseUser(User user) {
     return user != null ? MyUser(uid: user.uid) : null;
+  }
+
+  //auth change user stream
+  Stream<MyUser> get user {
+    return _auth
+        .authStateChanges()
+        .map((User? user) => _userFromFirebaseUser(user!)!);
   }
 
   // sign in anonymous
