bb377897bbf9347b57b1fb668aa0ecd2b8170ca8
Alxbry28 <46429925+Alxbry28@users.noreply.github.com>
2023-02-01 23:21:30 +0800

    [tutorial6] custom user model setup

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,15 +1,22 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:practicefirebase/models/user.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //Create user obj based on FirebaseUser
+  MyUser? _userFromFirebaseUser(User user){
+    return user != null ? MyUser(uid: user.uid) : null;
+  }
 
   // sign in anonymous
   Future signInAnonymous() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user;
+      // return user;
+      return _userFromFirebaseUser(user!);
     } catch (e) {
       print(e.toString());
       return null;
