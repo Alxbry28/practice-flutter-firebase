114ee237f8c9c731ece2274ab8539e999250ba89
Alxbry28 <46429925+Alxbry28@users.noreply.github.com>
2023-02-02 17:41:57 +0800

    [tutorial20] listing brew data

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:practicefirebase/models/brew.dart';
 import 'package:provider/provider.dart';
 
+import 'brew_tile.dart';
+
 class BrewList extends StatefulWidget {
   const BrewList({super.key});
 
@@ -13,19 +15,30 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<List<Brew?>?>(context);
-    if (brews != null) {
-      for (var brew in brews) {
-          print(brew?.name);
-          print(brew?.sugars);
-          print(brew?.strength);
-        }
-    }
+    final brews = Provider.of<List<Brew>?>(context);
+
+    // if (brews != null) {
+    //   int count = 1;
+    //   for (var brew in brews) {
+    //       print("${count}: ${brew?.name}");
+    //       print("${count}: ${brew?.sugars}");
+    //       print("${count}: ${brew?.strength}");
+    //     }
+    //     count++;
+    // }
+
     // if (brews != null) {
     //   for (var doc in brews.docs) {
     //     print(doc.data());
     //   }
     // }
-    return Container();
+
+    return ListView.builder(
+      itemCount: brews!.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew:brews[index]);
+      },
+    );
+    // return Container();
   }
 }
---
+++ lib/screens/home/brew_tile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import "package:practicefirebase/models/brew.dart";
+
+class BrewTile extends StatelessWidget {
+  const BrewTile({
+    Key? key,
+    required this.brew,
+  }) : super(key: key);
+
+  final Brew brew;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Card(
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 25.0,
+            backgroundColor: Colors.brown[brew.strength!],
+          ),
+          subtitle: Text("Takes ${brew.sugars} sugar(s)"),
+        ),
+        margin: EdgeInsets.fromLTRB(20, 6, 20, 0),
+      ),
+    );
+  }
+}
