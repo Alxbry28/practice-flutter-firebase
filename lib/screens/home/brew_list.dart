8bcb0a0a993bd18d162508e8b1b7a67f27d660d6
Alxbry28 <46429925+Alxbry28@users.noreply.github.com>
2023-02-03 09:12:37 +0800

    [tutorial25] Using a stream builder

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -15,7 +15,7 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<List<Brew>?>(context);
+    final brews = Provider.of<List<Brew>?>(context) ?? [];
 
     // if (brews != null) {
     //   int count = 1;
@@ -34,7 +34,7 @@ class _BrewListState extends State<BrewList> {
     // }
 
     return ListView.builder(
-      itemCount: brews!.length,
+      itemCount: brews.length,
       itemBuilder: (context, index) {
         return BrewTile(brew:brews[index]);
       },
--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,7 +1,11 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:practicefirebase/models/user.dart';
+import 'package:practicefirebase/services/firestoredb.dart';
 import 'package:practicefirebase/shared/constants.dart';
+import 'package:practicefirebase/shared/loading.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   const SettingsForm({super.key});
@@ -21,63 +25,74 @@ class _SettingsFormState extends State<SettingsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          const Text(
-            "Update your brew settings.",
-            style: TextStyle(fontSize: 18.0),
-          ),
-          const SizedBox(height: 20),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: (value) => value!.isEmpty ? "Please enter a name" : null,
-            onChanged: (value) => setState(() {
-              _currentName = value;
-            }),
-          ),
-          const SizedBox(height: 20),
-          //Dropdown
-          DropdownButtonFormField(
-            value: _currentSugars ?? "0",
-            hint: const Text("Select Sugar"),
-            items: sugars
-                .map((sugar) => DropdownMenuItem(
-                      value: sugar,
-                      child: Text("$sugar sugars"),
-                    ))
-                .toList(),
-            onChanged: (value) => setState(() {
-              _currentSugars = value;
-            }),
-          ),
+    final user = Provider.of<MyUser?>(context);
+    return StreamBuilder<UserData>(
+        stream: FirestoreDBService(uid: user!.uid).userData,
+        builder: (context, snapshot) {
+        
+          if (!snapshot.hasData) {
+            return const Loading();
+          }
+          UserData user_data = snapshot.data!;
+          return Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Text(
+                  "Update your brew settings.",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  initialValue: user_data.name,
+                  decoration: textInputDecoration,
+                  validator: (value) =>
+                      value!.isEmpty ? "Please enter a name" : null,
+                  onChanged: (value) => setState(() {
+                    _currentName = value;
+                  }),
+                ),
+                const SizedBox(height: 20),
+                //Dropdown
+                DropdownButtonFormField(
+                  value: _currentSugars ?? user_data.sugars,
+                  hint: const Text("Select Sugar"),
+                  items: sugars
+                      .map((sugar) => DropdownMenuItem(
+                            value: sugar,
+                            child: Text("$sugar sugars"),
+                          ))
+                      .toList(),
+                  onChanged: (value) => setState(() {
+                    _currentSugars = value;
+                  }),
+                ),
 
-          //Slider
-          Slider(
-            value: (_currentStrength ?? 100).toDouble(),
-            activeColor: Colors.brown[_currentStrength ?? 100],
-            inactiveColor: Colors.brown[_currentStrength ?? 100],
-            min: 100,
-            max: 900,
-            divisions: 8,
-            onChanged: (value) => setState(() {
-              _currentStrength = value.round();
-            }),
-          ),
+                //Slider
+                Slider(
+                  value: (_currentStrength ?? user_data.strength!).toDouble(),
+                  activeColor: Colors.brown[_currentStrength ?? user_data.strength!],
+                  inactiveColor: Colors.brown[_currentStrength ?? user_data.strength!],
+                  min: 100,
+                  max: 900,
+                  divisions: 8,
+                  onChanged: (value) => setState(() {
+                    _currentStrength = value.round();
+                  }),
+                ),
 
-          //Update button
-          ElevatedButton(
-            onPressed: () async {
-              print(_currentName);
-              print(_currentSugars);
-              print(_currentStrength);
-            },
-            child: const Text("Update"),
-          ),
-          
-        ],
-      ),
-    );
+                //Update button
+                ElevatedButton(
+                  onPressed: () async {
+                    print(_currentName);
+                    print(_currentSugars);
+                    print(_currentStrength);
+                  },
+                  child: const Text("Update"),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
