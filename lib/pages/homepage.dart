86ff4b253c4e3e6023089c20e5ed84ece2bb7d6d
Alxbry28 <46429925+Alxbry28@users.noreply.github.com>
2023-02-10 00:05:15 +0800

    [gdev][part1] snackbar, dialog, and bottom sheet from getx

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String title = "GetX Demo";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Text(title),
+      ),
+    );
+  }
+}
